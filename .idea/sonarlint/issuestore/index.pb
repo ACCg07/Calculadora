0bd06951a0ef03ecb6f4cc27d72aaaaa51d18bec
Andy Chen Cheng <AndyAChenCheng@gmail.com>
2024-04-18 00:27:18 +0200

    Resultado calculadora

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,14 @@
+
+Q
+!src/Calculadora1/Calculadora.java,8\c\8cab7b741bdb0cb84966571367e5201637486150
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+?
+Calculadora.iml,c\9\c920dd5d7ddb9ecebe42b9323c5b473428bef0fa
+Å
+Q.idea/sonarlint/securityhotspotstore/8/c/8cab7b741bdb0cb84966571367e5201637486150,c\8\c8c60c178378fe4e091208df0947a2b53ac2e0d1
---
+++ src/Calculadora1/Calculadora.java
@@ -0,0 +1,25 @@
+package Calculadora1;
+
+public class Calculadora {
+    public static void main(String[] args) {
+int numeroA;
+int numeroB;
+String signo;
+
+        System.out.printf("Hello and welcome!");
+     int A = 2;
+     int B = 8;
+
+     int resultado = A + B;
+     int resultado2 = A - B;
+     int resultado3 = A * B;
+     int resultado4 = A / B;
+
+        System.out.println("\nLa suma es " + resultado);
+        System.out.println("La resta es " + resultado2);
+        System.out.println("La multiplicacion es " + resultado3);
+        System.out.println("La division es " + resultado4);
+}
+
+        }
+
